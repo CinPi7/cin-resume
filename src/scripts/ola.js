abec5b206d27d74bfc48c7952b249121a90ea30c
Cinthia <cinthiapissetti@gmail.com>
2020-08-13 12:09:50 -0300

    first commit

---
+++ src/scripts/ola.js
@@ -0,0 +1,2 @@
+console.log("%cWelcome to my work in constant progress :)", "background-color: #FFDAC1; color: black");
+console.log("%cMade with ♥ by Cinpis!", "background-color: #FFDAC1; color: black");
